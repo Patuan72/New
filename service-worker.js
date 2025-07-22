d9672395997751f0094ef43d1a5de145fad69835
Patuan72 <ozoedtech@gmail.com>
2025-07-22 21:08:34 +0700

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,23 @@
-console.log('mock sw');
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open('pwa-cache').then(function(cache) {
+      return cache.addAll([
+    './service-worker.js',
+    'https://cdn.jsdelivr.net/npm/meyda/dist/web/meyda.min.js',
+        './index.html',
+        './style.css',
+        './script.js',
+        './unit1.json',
+        './unit2.json'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
+    })
+  );
+});
